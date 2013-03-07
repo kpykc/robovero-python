905ea680f26f6dbe3827a0b11aed96059d47e6da
Eric2 <eric2@gumstix.com>
2013-03-07 14:25:18 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+  name='Robovero',
+  version='1.0',
+  description='Robovero Library',
+  author='Neil MacMunn',
+  author_email='neil@gumstix.com',
+  url='http://www.robovero.org/',
+  packages=[
+    'robovero',
+  ],
+  requires=[
+    'pyserial',
+  ],
+  install_requires=[
+    'pyserial',
+  ],
+)
+
