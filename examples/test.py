06cfaac52adebcedc5e5ddd5028dd56388d7f07a
Eric2 <eric2@gumstix.com>
2013-05-21 11:50:19 -0700

    Add PWM Servo Setup Function
    - Require Firmware Support

---
+++ examples/test.py
@@ -0,0 +1,17 @@
+from robovero.extras import roboveroConfig,
+robovero.internals import RoboCaller
+
+def getServoAngle():
+	"""Get an angle from the user and calculate new duty cycle.
+	"""
+	user_angle = raw_input("New angle: ")
+	return user_angle
+
+def run():
+  roboveroConfig()
+  RoboCaller.call('PWM_SetBasePeriod', 'void', 2000)
+  RoboCaller.call('PWM_SetSpeed', 'void', 1, 10)
+  while True:
+    match_value = getServoAngle()
+    if match_value:
+    RoboCaller.call('PWM_SetSpeed', 'void', 1, match_value)
